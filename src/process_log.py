0e77dac576c0f19d6986f589f31ec819e08e6da1
Phil Nova <pnova8@gmail.com>
2017-04-02 13:04:57 -0700

    implement argument parser to accept filename arguments

--- src/process_log.py
+++ src/process_log.py
@@ -1,2 +1,16 @@
-// your Python code to implement the features could be placed here
-// note that you may use any language, there is no preference towards Python
+import argparse
+
+parser = argparse.ArgumentParser(description='Process hits from a server log.')
+parser.add_argument('input_server_log',
+    help='ASCII file containing server log of requests.')
+parser.add_argument('output_hosts',
+    help='Writeable text file to contain list of the top 10 most active host/IP addresses that have accessed the site.')
+parser.add_argument('output_hours',
+    help='Writeable text file to contain list of 10 resources that consume the most bandwidth on the site.')
+parser.add_argument('output_resources',
+    help='Writeable text file to contain list of the top 10 most frequently visited 60-minute periods.')
+parser.add_argument('output_blocked',
+    help='Writeable text file to contain potential security threats.')
+args = parser.parse_args()
+
+print(args.input_server_log)
